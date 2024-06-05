f1f8f74225de11edb5bab085ab3e9f19b75fb7ef
Lauri Himanen <lauri.himanen@gmail.com>
2024-06-05 14:27:46 +0300

    Improved parser test.

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -1,6 +1,8 @@
+# ruff: noqa: PLR2004
 import logging
 
 from nomad.datamodel import EntryArchive
+
 from nomad_tadf_molecules.parsers.tadf_molecules import TADFMoleculesParser
 
 
@@ -8,3 +10,8 @@ def test_parse_file():
     parser = TADFMoleculesParser()
     archive = EntryArchive()
     parser.parse('tests/data/molecule30.json', archive, logging.getLogger())
+
+    assert archive.data.photoluminescence_quantum_yield is None
+    assert archive.data.peak_emission_wavelength.magnitude == 493.0
+    assert archive.data.delayed_lifetime is None
+    assert archive.data.singlet_triplet_energy_splitting.magnitude == 0.17
--- tests/schema_packages/test_schema.py
+++ tests/schema_packages/test_schema.py
@@ -6,10 +6,10 @@ from nomad.client import normalize_all, parse
 
 def test_tadf_schema():
     test_file = os.path.join('tests', 'data', 'test.archive.yaml')
-    entry_archive = parse(test_file)[0]
-    normalize_all(entry_archive)
+    archive = parse(test_file)[0]
+    normalize_all(archive)
 
-    assert entry_archive.data.photoluminescence_quantum_yield == 0.1
-    assert entry_archive.data.peak_emission_wavelength.magnitude == 400
-    assert entry_archive.data.delayed_lifetime.magnitude == 3
-    assert entry_archive.data.singlet_triplet_energy_splitting.magnitude == 2
+    assert archive.data.photoluminescence_quantum_yield == 0.1
+    assert archive.data.peak_emission_wavelength.magnitude == 400
+    assert archive.data.delayed_lifetime.magnitude == 3
+    assert archive.data.singlet_triplet_energy_splitting.magnitude == 2
